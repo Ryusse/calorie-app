10d58819c104606267faaea11190d4009839fa1c
Joel <joelangeltw@gmail.com>
2022-06-10 23:31:54 -0500

    routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,17 +3,17 @@
 import { Food, Statistics, Register } from '@pages/';
 import { Home } from '@pages/Home';
 import { Login } from '@pages/Login';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const RoutesIndex = () => {
   return (
-    <HashRouter>
+    <Routes>
       <Route index element={<Home />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
       <Route path="Statistics" element={<Statistics />} />
       <Route path="Food" element={<Food />} />
       <Route path="*" element={<Navigate replace to="/404" />} />
-    </HashRouter>
+    </Routes>
   );
 };
